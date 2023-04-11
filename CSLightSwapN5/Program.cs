4ebe3c72de427ffc5fce8a11e5418d3931ec7384
dengt_by dengt_by <denis.wse.new@gmail.com>
2023-04-11 10:30:11 +0300

    Добавьте файлы проекта.

---
+++ CSLightSwapN5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace CSLightSwapN5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Петров";
+            string lastName = "Егор";
+            string bufferString;
+
+            Console.WriteLine($"Имя: {firstName}\n" +
+                              $"Фамилия: {lastName}");
+
+            bufferString = firstName;
+            firstName = lastName;
+            lastName = bufferString;
+
+            Console.WriteLine("\nИсправление ошибки...");
+            Console.WriteLine($"Имя: {firstName}\n" +
+                              $"Фамилия: {lastName}");
+
+            Console.ReadLine();
+        }
+    }
+}
